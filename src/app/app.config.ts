8c3d1b846734b2aac51c7dfa0ffb4081cfd16673
Grover Gonzalez <grovergonzalez8@gmail.com>
2025-09-22 16:25:33 -0400

    re-building with a clean architecture and scalable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,11 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { HttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(HttpClient)
   ]
 };
--- src/app/core/models/role.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Role {
-    id: number;
-    name: string;
-    description?: string;
-    can_make_entry: boolean;
-    can_generate_seals: boolean;
-    can_generate_letters: boolean;
-}
